0054e648d6af6d0bcf178c8a18cfe1c5e0e5f442
ILeonteva <ILeonteva@inno.tech>
2022-12-02 18:00:08 +0300

    проверка пути к файлу

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -5,5 +6,24 @@ public class Main {
         System.out.println("Введите текст и нажмите <Enter>: ");
         String text = new Scanner(System.in).nextLine();
         System.out.println("Длина текста " + text.length());
+        int count = 0;
+
+        while (true) {
+            System.out.println("Введите путь к файлу");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExists) {
+                System.out.println("Нет такого файла");
+                continue;
+            }
+            if (isDirectory) {
+                System.out.println("Указан путь к директории, а не файлу");
+                continue;
+            }
+            count++;
+            System.out.println(String.format("Путь указан верно. Это файл номер %d ", count));
+        }
     }
 }
